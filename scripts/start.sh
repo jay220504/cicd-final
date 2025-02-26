ac49b00000f1dea23774397e11c8a7fc2016d42a
jay220504 <jaysolanki0422@gmail.com>
2025-02-26 14:24:05 +0530

    done'

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Ensure the correct PATH
-export PATH=$HOME/.npm-global/bin:$PATH
-
 cd /home/ec2-user/app
-
-# Ensure correct permissions
-sudo chown -R ec2-user:ec2-user /home/ec2-user/app
-sudo chmod -R 755 /home/ec2-user/app
-
-# Run npm install as ec2-user
-npm install
-# Start the application with PM2
-pm2 start server.js
-pm2 stop all || true
-
-# Save PM2 process list so it restarts after reboot
-pm2 save
+nohup node server.js > server.log 2>&1 &
--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline! 🚀 Updated for testing');
+  res.send('Hello from CI/CD Pipeline! 🚀 Updated for testing.');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-  
