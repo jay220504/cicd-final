db8729ba1131866a5cadb04aedaee1aa87e4659c
jay220504 <jaysolanki0422@gmail.com>
2025-02-26 14:21:45 +0530

    done'

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/app
-npm install
-node server.js > server.log 2>&1 &
+nohup node server.js > server.log 2>&1 &
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline! 🚀 Updated for testing jay solanki.');
+  res.send('Hello from CI/CD Pipeline! 🚀 Updated for testing.');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
   
