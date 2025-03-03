9ef7f64ec2b2f2440d33ad212599ac9c7082a827
jay220504 <jaysolanki0422@gmail.com>
2025-03-03 16:22:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline! 🚀 Updated for testing jay solanki 0522...');
+  res.send('Hello from CI/CD Pipeline! 🚀 Updated for testing jay solanki 05...');
 });
 
 const PORT = process.env.PORT || 3000;
